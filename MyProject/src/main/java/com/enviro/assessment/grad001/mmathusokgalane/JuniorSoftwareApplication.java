ac9116aea65c291ce19d7d44af95aafd4b858756
Mmathuso Angelina <48378806+Meythusoo98@users.noreply.github.com>
2023-10-02 15:23:18 +0200

    Add files via upload
    New project to work with

---
+++ MyProject/src/main/java/com/enviro/assessment/grad001/mmathusokgalane/JuniorSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.mmathusokgalane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorSoftwareApplication.class, args);
+	}
+
+}
